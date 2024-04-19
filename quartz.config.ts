04336b001f4151fced3c40893d78a41881a3b12f
corihuang <cori.huang@gmail.com>
2024-04-18 23:47:31 -0700

    Quartz sync: Apr 18, 2024, 11:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Fridge Documentation",
+    pageTitle: "💎 Fridge Documentation",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
